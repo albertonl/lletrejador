13a7427507b62d4c2e750d012e3cde9af553f015
Alberto Navalon <alberto.navalonlillo@gmail.com>
2019-05-14 21:25:39 +0200

    Update lletrejador.py (working correctly)

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # lletrejador
 Lletrejador amb paraules aleatòries de català
 
-[![current version 0.1](https://img.shields.io/badge/Current%20version-0.1-brightgreen.svg)](http://proshare.epizy.com)
+[![current version 0.1](https://img.shields.io/badge/Current%20version-0.1-brightgreen.svg)](https://github.com/albertonl/lletrejador)
+[![python version 0.1](https://img.shields.io/badge/Python%20version-3-brightgreen.svg)](https://github.com/albertonl/lletrejador)
+
+[![status working correctly](https://img.shields.io/badge/Status-working%20correctly-brightgreen.svg)](https://github.com/albertonl/lletrejador)
+[![Bugs none detected](https://img.shields.io/badge/Bugs-None%20detected-orange.svg)](https://github.com/albertonl/lletrejador)
 
 
-Realitzat amb Python 3 (versions anteriors també funcionen) i el diccionari de català de la plataforma Apertium (https://github.com/apertium/apertium-cat/blob/master/apertium-cat.cat.dix).
+Realitzat amb Python 3 (les versions anteriors a aquesta són incompatibles) i el diccionari de català de la plataforma Apertium (https://github.com/apertium/apertium-cat/blob/master/apertium-cat.cat.dix).
 
 THE USE OF THIS SOFTWARE IS SUBJECT TO THE GNU GENERAL PUBLIC LICENSE v3.0 (https://github.com/albertonl/lletrejador/blob/master/LICENSE for further information and details).
--- lletrejador.py
+++ lletrejador.py
@@ -1,10 +1,14 @@
-# Lletrejador de Català
+	#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
+
+# Lletrejador de català
 # Alberto Navalon Lillo (C) May 2019
 # This software is subject to the GNU General Public License
 # For further information and details, visit the repository page:
 # https://github.com/albertonl/lletrejador
 
-
+from xml.dom import minidom # To parse the document
+import subprocess as sp # Subprocesses
 import random # To use the random.randint() function to generate a random index to give a word
 
 # We parse all elements in Catalan dictionary
@@ -13,10 +17,10 @@ elist = xmldoc.getElementsByTagName('e')
 
 dicts = []
 for e in elist:
-    d = {}
-    for a in e.attributes.values():
-        d[a.name] = a.value
-    dicts.append(d)
+	d = {}
+	for a in e.attributes.values():
+		d[a.name] = a.value
+	dicts.append(d)
 
 
 
@@ -24,9 +28,81 @@ finished = False
 answer = 's'
 # Main loop
 while finished==False:
-    print(dicts[random.randint(0,len(dicts)-1)]['lm']);
-
-    answer = input("Vols continuar? (s/n): ")
-    if answer=='n':
-        finished = True
-        quit()
+	sp.call('clear',shell=True)
+	ran_num = random.randint(0,len(dicts)-1)
+	if 'lm' in dicts[ran_num].keys():
+		word = dicts[ran_num]['lm']
+	else:
+		continue
+	print(word)	
+	print("\n")
+	word = word.lower()
+	for i in range(0,len(word)):
+		if word[i]==' ':
+			print(" ", end=" ")
+		elif word[i]=='a':
+			print("a", end=" ")
+		elif word[i]=='b':
+			print("be", end=" ")
+		elif word[i]=='c':
+			print("ce", end=" ")
+		elif word[i]=='d':
+			print("de", end=" ")
+		elif word[i]=='e':
+			print("e", end=" ")
+		elif word[i]=='f':
+			print("efe", end=" ")
+		elif word[i]=='g':
+			print("ge", end=" ")
+		elif word[i]=='h':
+			print("hac", end=" ")
+		elif word[i]=='i':
+			print("i", end=" ")
+		elif word[i]=='j':
+			print("jota", end=" ")
+		elif word[i]=='k':
+			print("ca", end=" ")
+		elif word[i]=='l':
+			if word[i+1]=='·' and word[i+2]=='l' and i<(len(word)-2): # it could give a 'string index out of range' error
+				print("ele geminada", end=" ")
+			else:
+				print("ele", end=" ")
+		elif word[i]=='m':
+			print("eme", end=" ")
+		elif word[i]=='n':
+			print("ene", end=" ")
+		elif word[i]=='o':
+			print("o", end=" ")
+		elif word[i]=='p':
+			print("pe", end=" ")
+		elif word[i]=='q':
+			print("cu", end=" ")
+		elif word[i]=='r':
+			print("erre", end=" ")
+		elif word[i]=='s':
+			print("esse", end=" ")
+		elif word[i]=='t':
+			print("te", end=" ")
+		elif word[i]=='u':
+			print("u", end=" ")
+		elif word[i]=='v':
+			print("ve (baixa)", end=" ")
+		elif word[i]=='w':
+			print("ve doble", end=" ")
+		elif word[i]=='x':
+			print("ics", end=" ")
+		elif word[i]=='y':
+			print("i grega", end=" ")
+		elif word[i]=='z':
+			print("zeta", end=" ")
+		else:
+			print("??", end=" ")
+		
+		if i<(len(word)-1):
+			print("-", end=" ")
+	
+	print("\n")
+	answer = input("Vols continuar? (s/n): ")
+	if answer=='n':
+		finished = True
+		quit()
